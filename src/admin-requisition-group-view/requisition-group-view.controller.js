af29cb7d8f03f2930c8f75ae97c27052b2844d98
saleksandra <soltys.ola@gmail.com>
2021-05-20 12:31:39 +0200

    SELV3-337: Added requisition group files from Core project

---
+++ src/admin-requisition-group-view/requisition-group-view.controller.js
@@ -0,0 +1,129 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc controller
+     * @name admin-requisition-group-view.controller:RequisitionGroupViewController
+     *
+     * @description
+     * Controller for managing requisition group view screen.
+     */
+    angular
+        .module('admin-requisition-group-view')
+        .controller('RequisitionGroupViewController', controller);
+
+    controller.$inject = ['$state', '$stateParams', 'requisitionGroup', 'memberFacilities'];
+
+    function controller($state, $stateParams, requisitionGroup, memberFacilities) {
+
+        var vm = this;
+
+        vm.$onInit = onInit;
+        vm.searchForFacilities = searchForFacilities;
+
+        /**
+         * @ngdoc property
+         * @propertyOf admin-requisition-group-view.controller:RequisitionGroupViewController
+         * @name requisitionGroup
+         * @type {Object}
+         *
+         * @description
+         * Contains requisition group object.
+         */
+        vm.requisitionGroup = undefined;
+
+        /**
+         * @ngdoc property
+         * @propertyOf admin-requisition-group-view.controller:RequisitionGroupViewController
+         * @name facilityName
+         * @type {String}
+         *
+         * @description
+         * Contains requisition group object.
+         */
+        vm.facilityName = undefined;
+
+        /**
+         * @ngdoc property
+         * @propertyOf admin-requisition-group-view.controller:RequisitionGroupViewController
+         * @name memberFacilities
+         * @type {Array}
+         *
+         * @description
+         * Contains paginated list of all requisition group associated facilities.
+         */
+        vm.memberFacilities = undefined;
+
+        /**
+         * @ngdoc property
+         * @propertyOf admin-requisition-group-view.controller:RequisitionGroupViewController
+         * @name memberFacilitiesPage
+         * @type {Array}
+         *
+         * @description
+         * Contains current page of associated facilities.
+         */
+        vm.memberFacilitiesPage = undefined;
+
+        /**
+         * @ngdoc property
+         * @propertyOf admin-requisition-group-view.controller:RequisitionGroupViewController
+         * @name selectedType
+         * @type {Number}
+         *
+         * @description
+         * Contains number of currently selected tab.
+         */
+        vm.selectedType = undefined;
+
+        /**
+         * @ngdoc method
+         * @methodOf admin-requisition-group-view.controller:RequisitionGroupViewController
+         * @name $onInit
+         *
+         * @description
+         * Method that is executed on initiating RequisitionGroupViewController.
+         */
+        function onInit() {
+            vm.requisitionGroup = requisitionGroup;
+            vm.memberFacilities = memberFacilities;
+            vm.facilityName = $stateParams.facilityName;
+            vm.selectedTab = $stateParams.tab ? parseInt($stateParams.tab) : 0;
+        }
+
+        /**
+         * @ngdoc method
+         * @methodOf admin-requisition-group-view.controller:RequisitionGroupViewController
+         * @name search
+         *
+         * @description
+         * Reloads page with new search parameters.
+         */
+        function searchForFacilities() {
+            var stateParams = angular.copy($stateParams);
+
+            stateParams.facilityName = vm.facilityName;
+            stateParams.tab = 1;
+
+            $state.go('openlmis.administration.requisitionGroupView', stateParams, {
+                reload: true
+            });
+        }
+    }
+})();
---
+++ src/admin-requisition-group-view/requisition-group-view.controller.spec.js
@@ -0,0 +1,100 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+describe('RequisitionGroupViewController', function() {
+
+    beforeEach(function() {
+        module('admin-requisition-group-view');
+
+        inject(function($injector) {
+            this.$controller = $injector.get('$controller');
+            this.$state = $injector.get('$state');
+            this.RequisitionGroupDataBuilder = $injector.get('RequisitionGroupDataBuilder');
+            this.FacilityDataBuilder = $injector.get('FacilityDataBuilder');
+        });
+
+        this.requisitionGroup = new this.RequisitionGroupDataBuilder().buildJson();
+
+        this.memberFacilities = [
+            new this.FacilityDataBuilder().build(),
+            new this.FacilityDataBuilder().build()
+        ];
+
+        this.stateParams = {
+            page: 0,
+            size: 10,
+            id: 'group-id',
+            tab: 0,
+            facilityName: 'facility'
+        };
+
+        this.vm = this.$controller('RequisitionGroupViewController', {
+            requisitionGroup: this.requisitionGroup,
+            memberFacilities: this.memberFacilities,
+            $stateParams: this.stateParams
+        });
+        this.vm.$onInit();
+
+        spyOn(this.$state, 'go').andReturn();
+    });
+
+    describe('onInit', function() {
+
+        it('should expose requisition group', function() {
+            expect(this.vm.requisitionGroup).toEqual(this.requisitionGroup);
+        });
+
+        it('should expose member facilities', function() {
+            expect(this.vm.memberFacilities).toEqual(this.memberFacilities);
+        });
+
+        it('should expose facility name', function() {
+            expect(this.vm.facilityName).toEqual(this.stateParams.facilityName);
+        });
+
+        it('should expose selected tab', function() {
+            expect(this.vm.selectedTab).toEqual(this.stateParams.tab);
+        });
+
+        it('should expose search for facilities method', function() {
+            expect(angular.isFunction(this.vm.searchForFacilities)).toBe(true);
+        });
+    });
+
+    describe('searchForFacilities', function() {
+
+        it('should set facility name param', function() {
+            this.vm.facilityName = 'some-name';
+
+            this.vm.searchForFacilities();
+
+            expect(this.$state.go).toHaveBeenCalledWith('openlmis.administration.requisitionGroupView', {
+                page: this.stateParams.page,
+                size: this.stateParams.size,
+                id: this.stateParams.id,
+                tab: 1,
+                facilityName: 'some-name'
+            }, {
+                reload: true
+            });
+        });
+
+        it('should call state go method', function() {
+            this.vm.searchForFacilities();
+
+            expect(this.$state.go).toHaveBeenCalled();
+        });
+    });
+});
