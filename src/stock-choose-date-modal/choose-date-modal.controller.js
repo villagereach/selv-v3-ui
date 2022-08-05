4943213b9d58372f48adf99aaae9a809240f685b
sniedzielski <sniedzielski@soldevelo.com>
2022-08-05 15:21:04 +0200

    SELV3-507: Allow user to enter Shipment Date

---
+++ src/stock-choose-date-modal/choose-date-modal.controller.js
@@ -0,0 +1,54 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc controller
+     * @name stock-choose-date-modal.controller:ChooseDateModalController
+     *
+     * @description
+     * Manages Choose Date Modal.
+     */
+    angular
+        .module('stock-choose-date-modal')
+        .controller('ChooseDateModalController', controller);
+
+    controller.$inject = ['$filter', 'modalDeferred', 'authorizationService'];
+
+    function controller($filter, modalDeferred, authorizationService) {
+        var vm = this;
+
+        vm.maxDate = $filter('isoDate')(new Date());
+        vm.occurredDate = vm.maxDate;
+        // SELV3-507: Allow user to enter Shipment Date
+        vm.shipmentDate = vm.maxDate;
+        vm.signature = '';
+        vm.username = authorizationService.getUser().username;
+
+        vm.submit = function() {
+            if (vm.occurredDate) {
+                modalDeferred.resolve({
+                    occurredDate: vm.occurredDate,
+                    signature: vm.signature,
+                    shipmentDate: vm.shipmentDate
+                });
+            }
+        };
+        // SELV3-507: ends here
+    }
+})();
