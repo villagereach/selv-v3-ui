cd17108d654bd85838a93371b9117282f1139bed
sniedzielski <sniedzielski@soldevelo.com>
2023-01-24 16:15:32 +0100

    SELV3-554: fixed indentation

--- src/admin-dhis2/components/AdminDhis2ServersPage/AdminDhis2ServersPage.jsx
+++ src/admin-dhis2/components/AdminDhis2ServersPage/AdminDhis2ServersPage.jsx
@@ -140,7 +140,7 @@ const AdminDhis2ServersPage = () => {
                         refetch={fetchServersList}
                     />
                 ]}
-      />
+            />
         </>
     )
 };
