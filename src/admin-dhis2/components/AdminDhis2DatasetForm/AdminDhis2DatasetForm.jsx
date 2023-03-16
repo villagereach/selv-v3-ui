ca36c77faafa541cc89e6893100a8b2c35bb229e
mdulko <122530805+mdulko@users.noreply.github.com>
2023-03-16 10:01:51 +0100

    SELV3-559: update to add dataset functionality (#23)
    * SELV3-559: update to add dataset functionality
    
    * SELV3-559: added consts file

--- src/admin-dhis2/components/AdminDhis2DatasetForm/AdminDhis2DatasetForm.jsx
+++ src/admin-dhis2/components/AdminDhis2DatasetForm/AdminDhis2DatasetForm.jsx
@@ -1,98 +1,236 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 
-import { Form } from 'react-final-form';
-import arrayMutators from 'final-form-arrays';
-import { FieldArray } from 'react-final-form-arrays';
+import { toast } from 'react-toastify';
 
 import getService from '../../../react-components/utils/angular-utils';
-import SelectField from '../../../react-components/form-fields/select-field';
-import AddButton from '../../../react-components/buttons/add-button';
+import { SearchSelect } from '../../../requisition-order-create/search-select'
+import {CRON_EXPRESSION_OPTIONS, WEEKLY_OPTIONS, DAY_OPTIONS, HOUR_OPTIONS, MINUTE_OPTIONS} from "../../consts";
 
-function AdminDhis2DatasetForm({
-    onSubmit, onCancel, 
-    title, initialFormValue, 
-    mode, refetch
-}) {
+function AdminDhis2DatasetForm({ onSubmit, onCancel, refetch, serverId }) {
 
-  // TODO - here fetch from backend datasets that the DHIS2 user has access 
-  const datasetOptions = [];  
+    const [datasetOptions, setDatasetOptions] = useState([]);
 
-  const validate = values => {
-    const errors = { items: [] };
+    const [dhisDataset, setDhisDataset] = useState('');
+    const [selectedDataset, setSelectedDataset] = useState("");
+    const [selectedCronExpression, setSelectedCronExpression] = useState("");
+    const [timeOffset, setTimeOffset] = useState(0);
+    const [day, setDay] = useState(1);
+    const [hour, setHour] = useState(0);
+    const [minutes, setMinutes] = useState(0);
 
-    return errors;
-};
+    const serverService = useMemo(
+        () => {
+            return getService('adminDhis2');
+        },
+        []
+    );
 
-  const serverService = useMemo(
-      () => {
-          return getService('adminDhis2');
-      },
-      []
-  );
+    const fetchDatasets = () => {
+        if (serverId) {
+            serverService?.getServerDatasetsList(serverId)
+                .then((fetchedDatasets) => {
+                    const {content} = fetchedDatasets;
+
+                    const datasets = content.map((dataset) => ({
+                        name: dataset.displayName,
+                        value: dataset.displayName,
+                        id: dataset.id
+                    }));
+                    setDatasetOptions(datasets);
+                });
+        }
+    }
+
+    useEffect(() => fetchDatasets(), [serverId]);
+
+    const selectNumber = (value, countFrom = 0) => {
+
+        const options = [];
+        let n = countFrom;
+
+        for (n; n <= value ; n++) {
+            options.push({ name: `${n}`, value: n });
+        }
+
+        return options;
+    }
+
+    const selectDate = () => (
+        <>
+            {selectedCronExpression === CRON_EXPRESSION_OPTIONS[0].value &&
+                <>
+                    <span> on day </span>
+                    <SearchSelect
+                        options={selectNumber(DAY_OPTIONS, 1)}
+                        value={day}
+                        onChange={value => setDay(value)}
+                        placeholder=''
+                    />
+                    <span> of month at </span>
+                    <SearchSelect
+                        options={selectNumber(HOUR_OPTIONS)}
+                        value={hour}
+                        onChange={value => setHour(value)}
+                        placeholder=''
+                    />
+                    <span> : </span>
+                    <SearchSelect
+                        options={selectNumber(MINUTE_OPTIONS)}
+                        value={minutes}
+                        onChange={value => setMinutes(value)}
+                        placeholder=''
+                    />
+                </>
+            }
+            {selectedCronExpression === 'WEEKLY' &&
+                <>
+                    <span> on day </span>
+                    <SearchSelect
+                        options={selectNumber(WEEKLY_OPTIONS, 1)}
+                        value={day}
+                        onChange={value => setDay(value)}
+                        placeholder=''
+                    />
+                    <span> at </span>
+                    <SearchSelect
+                        options={selectNumber(HOUR_OPTIONS)}
+                        value={hour}
+                        onChange={value => setHour(value)}
+                        placeholder=''
+                    />
+                    <span> : </span>
+                    <SearchSelect
+                        options={selectNumber(MINUTE_OPTIONS)}
+                        value={minutes}
+                        onChange={value => setMinutes(value)}
+                        placeholder=''
+                    />
+                </>
+            }
+            {selectedCronExpression === 'DAILY' &&
+                <>
+                    <span> at </span>
+                    <SearchSelect
+                        options={selectNumber(HOUR_OPTIONS)}
+                        value={hour}
+                        onChange={value => setHour(value)}
+                        placeholder=''
+                    />
+                    <span> : </span>
+                    <SearchSelect
+                        options={selectNumber(MINUTE_OPTIONS)}
+                        value={minutes}
+                        onChange={value => setMinutes(value)}
+                        placeholder=''
+                    />
+                </>
+            }
+        </>
+    )
+
+    useEffect(() => {
+        setDhisDataset(datasetOptions.find(dataset => dataset.name === selectedDataset));
+    }, [selectedDataset])
+
+    const resetSchedule = () => {
+        setDay(1);
+        setHour(0);
+        setMinutes(0);
+    }
+
+    const setInitialValues = () => {
+        setSelectedDataset('');
+        setSelectedCronExpression('');
+        resetSchedule();
+    }
+
+    useEffect(() => {
+        if (selectedCronExpression === CRON_EXPRESSION_OPTIONS[0].value
+            || selectedCronExpression === CRON_EXPRESSION_OPTIONS[1].value) {
+            const adjustedDay = day - 1;
+            const onDay = 1440 * adjustedDay;
+            const onTime = hour * 60 + minutes;
+            setTimeOffset(onDay + onTime);
+        }
+        else if (selectedCronExpression === CRON_EXPRESSION_OPTIONS[2].value) {
+            setTimeOffset(hour * 60 + minutes);
+        }
+
+    }, [day, hour, minutes, timeOffset])
+
+    const submitDataElement = () => {
+
+        const dataset = {
+            name: selectedDataset,
+            cronExpression: selectedCronExpression,
+            serverDto: { id: serverId },
+            dhisDatasetId: dhisDataset.id,
+            timeOffset: timeOffset
+        }
+
+        serverService.addDataset(serverId, dataset)
+            .then(() => {
+                refetch();
+                onSubmit();
+                toast.success('Dataset has been added successfully!');
+                setInitialValues();
+            });
+    }
 
   return (
-    <div style={{marginBottom: "40px"}}>
-        <Form
-            initialValues={{ items: initialFormValue }}
-            onSubmit={onSubmit}
-            validate={validate}
-            mutators={{ ...arrayMutators }}
-            render={({ handleSubmit, values, invalid }) => (
-                <form className="form-container" onSubmit={handleSubmit}>
-                    <FieldArray name="items">
-                        {({ fields }) => (                       
-                            <div className="form-container">
-                                <div className="page-header-responsive">
-                                    <div id="header-wrap" style={{marginBottom: "24px"}}>
-                                        <h2 id="product-add-header">
-                                          {title}
-                                        </h2>
-                                    </div>
-                                </div>
-                                <div className="form-body">
-                                    {fields.map((name, index) => (
-                                        <div key={name}>
-                                            <SelectField
-                                                required
-                                                name={`${name}.dataset`}
-                                                label="Dataset"
-                                                options={datasetOptions}
-                                                objectKey="id"
-                                                containerClass='field-full-width required'
-                                            />
-                                        </div>
-                                    ))}
-                                </div>
-                                
-                                <div className="navbar">
-                                    <div id='navbar-wrap'>
-                                        <div>
-                                            <button 
-                                                type="button" 
-                                                className="secondary" 
-                                                onClick={onCancel}
-                                            >
-                                                <span>Cancel</span>
-                                            </button>
-                                        </div>                                
-                                        <div>
-                                            <AddButton
-                                                className="primary"
-                                                disabled={invalid}
-                                                onClick={() => {}}
-                                            >
-                                                Add
-                                            </AddButton>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        )}
-                    </FieldArray>
-                </form>
-            )}
-        />
-    </div>
+      <div className="page-container">
+          <div className="page-header-responsive">
+              <h2>Add Dataset</h2>
+          </div>
+          <div className="page-content element-create-form">
+              <div className='section field-full-width'>
+                  <div><strong className="is-required">Dataset</strong></div>
+                  <SearchSelect
+                      options={datasetOptions}
+                      value={selectedDataset}
+                      onChange={value => setSelectedDataset(value)}
+                      placeholder="Select dataset"
+                  />
+              </div>
+              <div className='section field-full-width date-section'>
+                  <div><strong className="is-required">Schedule</strong></div>
+                  <SearchSelect
+                      options={CRON_EXPRESSION_OPTIONS}
+                      value={selectedCronExpression}
+                      onChange={value => {
+                          setSelectedCronExpression(value);
+                          resetSchedule();
+                      }}
+                      placeholder="Select schedule"
+                  />
+                  <div className='time-section'>{selectDate()}</div>
+              </div>
+              <div className="bottom-bar">
+                  <div>
+                      <button
+                          type="button"
+                          className="secondary"
+                          onClick={() => {
+                              onCancel();
+                              setInitialValues();
+                          }}
+                      >
+                          <span>Cancel</span>
+                      </button>
+                  </div>
+                  <div>
+                      <button
+                          className="primary"
+                          type="button"
+                          disabled={!selectedDataset || !selectedCronExpression}
+                          onClick={() => submitDataElement()}
+                      >
+                          Add
+                      </button>
+                  </div>
+              </div>
+          </div>
+      </div>
 );
 }
 
